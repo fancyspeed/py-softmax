24b1c30b187a4b956d49f14e8fcfd4457c81356f
Zuotao Liu <fancyspeed@163.com>
2014-08-05 01:37:35 +0800

    finish coding

--- softmax.py
+++ softmax.py
@@ -2,6 +2,7 @@
 import sys
 import argparse
 import random
+import math
 
 class Softmax(object):
     def __init__(self):
@@ -38,11 +39,16 @@ class Softmax(object):
         print 'max feat idx', self.max_word_idx
 
     def load_line(self, line):
-        labels, feats = [], {} 
+        labels, feats = {}, {} 
         if not line.strip() or line[0] == '#':
             return labels, feats
+
         row = line.rstrip().split(' ')
-        labels = [v for v in row[0].split(',') if v]
+        if row[0].find(':') < 0:
+            labels = {v:1 for v in row[0].split(',') if v}
+        else:
+            labels = {v.split(':')[0]:float(v.split(':')[1]) for v in row[0].split(',') if v}
+
         feat_pairs = row[1:]
         for pair in feat_pairs:
             word_idx, word_count = pair.split(':') 
@@ -51,13 +57,36 @@ class Softmax(object):
 
     def predict(self, feats):
         labels = {}
+        tot = 0
+        for label in self.label_dict:
+            labels[label] = 0
+            for word_idx in feats:
+                labels[label] += feats[word_idx] * self.label_dict[label][word_idx]
+            labels[label] = math.exp(labels[label])
+            tot += labels[label]
+        if tot <= 0: return labels
+        for label in labels:
+            labels[label] /= tot
         return labels 
 
-    def gradent(self, labels, feats, alpha=0.001, eta=0.01):
-        return 0
+    def gradent_descent(self, labels, feats, alpha=0.001, eta=0.01):
+        predicts = self.predict(feats)
+        loss = 0
+        for label in labels:
+            grad = - labels[label] * (1 - predicts[label])
+            for word_idx in feats:
+                update = grad * feats[word_idx] + eta * self.label_dict[label][word_idx]
+                self.label_dict[label][word_idx] -= alpha * update 
+            loss += - predicts[label] * labels[label]
+        return loss 
 
-    def get_error(self, labels, feats):
-        return 0
+    def get_error(self, labels, feats, predicts={}):
+        loss = 0 
+        if not predicts:
+            predicts = self.predict(feats)
+        for label in labels:
+            loss += - predicts[label] * labels[label]
+        return loss 
 
     def train(self, p_train, alpha=0.001, eta=0.01, epsion=0.0001, max_iter=50):
         last_rmse, this_rmse = self.doc_num * 10000, self.doc_num * 10000
@@ -65,14 +94,14 @@ class Softmax(object):
             train_rmse = 0
             for line in open(p_train): 
                 labels, feats = self.load_line(line)
-                error = self.gradent(labels, feats, alpha, eta)
+                error = self.gradent_descent(labels, feats, alpha, eta)
                 train_rmse += error 
             this_rmse = 0
             for line in open(p_train): 
                 labels, feats = self.load_line(line)
                 error = self.get_error(labels, feats)
                 this_rmse += error 
-            print 'train rmse:%s, this rmse:%s, last rmse:%s' % (train_rmse, this_rmse, last_rmse)
+            print 'train loss:%s, this loss:%s' % (train_rmse, this_rmse)
             if last_rmse - this_rmse <= epsion:
                 break
             last_rmse = this_rmse
@@ -94,10 +123,14 @@ if __name__ == '__main__':
     parser.add_argument('-train', required=True)
     parser.add_argument('-test', required=True)
     parser.add_argument('-o', '--output', required=True)
+    parser.add_argument('-alpha',  type=float, default=0.001)
+    parser.add_argument('-eta',  type=float, default=0.01)
+    parser.add_argument('-epsion',  type=float, default=0.0001)
+    parser.add_argument('-iter', dest='max_iter', type=int, default=50, help='total iteration')
     args = parser.parse_args()
 
     softmax = Softmax()
     softmax.init_model(args.train)
-    softmax.train(args.train, 0.001, 0.01, 0.0001, 50)
+    softmax.train(args.train, args.alpha, args.eta, args.epsion, args.max_iter)
     softmax.infer(args.test, args.output)
 
